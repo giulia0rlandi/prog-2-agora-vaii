9242ae8946740f561eec39c8147dace72e3e500b
giulia0rlandi <giugiu.orlandi@gmail.com>
2024-08-12 07:53:50 -0300

    Add files via upload
    PROG AULA 1 - 12/08/2024

---
+++ perimTriACompletar.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  5 23:46:05 2023
+
+@author: Ferlin
+
+"""
+'''
+1) Tarefa obter pontos
+        Perguntar coordenadas dos pontos ao usuário
+        Não pode ser função pq precisa retornar mais de um valor
+
+2) Tarefa calcular perímetro  (função)
+        E: ptos A,B,C
+        S: perímetro
+        Proc: 
+            somar calc dist AB, calc dist AC, calc dist BC
+        
+3)Tarefa exibir perímetro: (função)
+        E: perímetro
+        S: nada
+        Proc: 
+            mostrar (exibir no monitor) moldura,perímero,moldura
+
+Funçoes auxilares para execução das tarefas
+A) calcula distância entre dois pontos
+        E: pto1, pto2
+        S: distãncia
+        Proc: 
+            fórmula da distância euclidiana
+
+B) exibir moldura
+        E: qtdade e símbolo
+        S: nada
+        Proc: 
+            exibir qtdade vezes do símbolo no monitor
+        
+C) Tarefa verificar se 3 pontos são colinares 
+        E: ptos A,B,C
+        S: True/False
+            -->Teste de  Colinear: ​   DP – DS == 0​
+                DP = xA*yB  +  yA*xC  +  xB*yC​
+                DS = yA*xB  +  xA*yC  +  yB*xC
+    
+'''
+# def calcDistancia(x1,y1,x2,y2):
+    
+
+# def calcPerim(xA,yA,xB,yB,xC,yC):  
+    
+# def moldura():
+    
+
+# def exibeRes(perim):
+
+
+#def saoColineares(xA,yA,xB,yB,xC,yC): 
+        
+# #BP
